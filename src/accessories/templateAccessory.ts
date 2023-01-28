a275575be5ed87da18e72723fe604b7bf9b51b8f
ptysell <9489667+ptysell@users.noreply.github.com>
2023-01-28 13:04:51 -0700

    Template Build

--- src/accessories/templateAccessory.ts
+++ src/accessories/templateAccessory.ts
@@ -15,32 +15,20 @@ export class vdpTemplateAccessory {
     private readonly accessory: PlatformAccessory,
   ) {
 
-    this.accessorySettings.Name = 'ABCD';
-
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, MANUFACTURER_NAME)
       .setCharacteristic(this.platform.Characteristic.Model, PLATFORM_NAME)
       .setCharacteristic(this.platform.Characteristic.SerialNumber, this.accessorySettings.Name);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
     this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
 
   }
-
 
   async setOn(value: CharacteristicValue) {
     this.accessorySettings.Status = value as boolean;
@@ -52,7 +40,5 @@ export class vdpTemplateAccessory {
     this.platform.log.debug('Get Characteristic On ->', isOn);
     return isOn;
   }
-
-
 
 }
