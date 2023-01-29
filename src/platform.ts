34eddc6a0bf1853d3126f39e6ec86f2192b62586
ptysell <9489667+ptysell@users.noreply.github.com>
2023-01-29 15:59:39 -0700

    Test Build

--- src/platform.ts
+++ src/platform.ts
@@ -78,6 +78,8 @@ export class vdpPlatform implements DynamicPlatformPlugin {
       const existingAccessory = deviceList.find(accessory => accessory.uuid === device.UUID);
       if(!existingAccessory){
         this.log.info('Pruning Platform Accessory:', device.displayName, 'at index', index);
+        //this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, existingAccessory);
+        this.accessories.splice(index, 1);
       }
       index++;
     }
