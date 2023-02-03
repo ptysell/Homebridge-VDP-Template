7ed44895fdb298f6d98df70077e48280b661cece
ptysell <9489667+ptysell@users.noreply.github.com>
2023-02-03 12:35:48 -0700

    Test uild

--- src/platformConfigurationManager.ts
+++ src/platformConfigurationManager.ts
@@ -67,15 +67,46 @@ export class platformConfigurationManager {
 
 
         if (this.update()) {
-          this.log.info('Configuration File Change: No');
-          this.refresh = false;
-        } else {
           this.log.info('Configuration File Change: Yes');
           const configData = fs.readFileSync(HOMEBRIDGE_CONFIGURATION_PATH, 'utf-8');
           const configFile: platformConfiguration = JSON.parse(configData);
+          this.log.error('----------Start Bridge----------');
+          this.log.error('Bridge Name:', configFile.bridge.name);
+          this.log.error('Bridge User Name:', configFile.bridge.username);
+          this.log.error('Bridge Port:', configFile.bridge.port);
+          this.log.error('Bridge Pin:', configFile.bridge.pin);
+          this.log.error('Bridge Advertiser:', configFile.bridge.advertiser);
+          this.log.error('Bridge Bind:', configFile.bridge.bind.toString);
+          this.log.error('----------End Bridge----------');
+
+
           for (let index=0; index < configFile.platforms.length; index++){
-            this.log.warn('Platform:', configFile.platforms[index].name);
+            this.log.warn('Platform:', configFile.platforms[index].platform.toString);
           }
+
+
+
+
+          // var results []map[string]interface{}
+
+          // // Unmarshal or Decode the JSON to the interface.
+          // json.Unmarshal([]byte(empArray), &results)
+
+          // for key, result := range results {
+          //   address := result["address"].(map[string]interface{})
+          //   fmt.Println("Reading Value for Key :", key)
+          //   //Reading each value by its key
+          //   fmt.Println("Id :", result["id"],
+          //     "- Name :", result["name"],
+          //     "- Department :", result["department"],
+          //     "- Designation :", result["designation"])
+          //   fmt.Println("Address :", address["city"], address["state"], address["country"])
+          // }
+
+
+
+        } else {
+          this.log.info('Configuration File Change: No');
 
         }
         setTimeout(() => {
